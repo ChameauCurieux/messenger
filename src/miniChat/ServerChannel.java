676f7da96e2eb192dc0c43f6c515074e113c0ec8
ChameauCurieux <qzdrg0123@gmail.com>
2019-03-04 16:13:32 +0100

    added client name & random messages + better connection closure server-side

--- src/miniChat/ServerChannel.java
+++ src/miniChat/ServerChannel.java
@@ -20,8 +20,16 @@ public class ServerChannel {
 	ServerSocketChannel serverChannel;
 	Thread handler;
 	boolean isRunning = false;
+	//List<String> waitingMessages; TODO keep a list of messages to send
+	String messageReceived;
 
+	/**
+	 * Creates a local server bound to the given port.
+	 * It uses NIO channels to communicate with the clients.
+	 * @param port : where the server accepts connections
+	 */
 	public ServerChannel(int port) {
+		//waitingMessages = new ArrayList<String>();
 		try {
 			// create new selector
 			selector = Selector.open();
@@ -39,67 +47,15 @@ public class ServerChannel {
 		}
 	}
 
+	/**
+	 * Launch the server handler
+	 */
 	public void startServer() {
 		try {
 			System.out.println("Starting server " + serverChannel.getLocalAddress());
 			isRunning = true;
 			// creating new handler
-			handler = new Thread(new Runnable () {
-
-				@Override
-				public void run() {
-					while(isRunning) {
-						try {
-							selector.select();
-							// checks whether the selector is open
-							if (!selector.isOpen()) {
-								break;
-							}
-							Set<SelectionKey> selectedKeys = selector.selectedKeys();
-							Iterator<SelectionKey> iterator = selectedKeys.iterator();
-
-							while (iterator.hasNext()) {
-								SelectionKey key = iterator.next();
-								iterator.remove();
-
-								if(key.isAcceptable()) {
-									// creating new connection
-									SocketChannel clientChannel = serverChannel.accept();
-									clientChannel.configureBlocking(false);
-									// adding it to the selector
-									clientChannel.register(selector, clientChannel.validOps());
-									System.out.println("Added client " + clientChannel.getRemoteAddress());
-								}
-
-								if(key.isConnectable()) {
-									((SocketChannel)key.channel()).finishConnect();
-								}
-
-								if(key.isReadable()) {
-									// read message
-									SocketChannel clientChannel = (SocketChannel) key.channel();
-									ByteBuffer buffer = ByteBuffer.allocate(256);
-									int nbBytesRead = clientChannel.read(buffer);
-									// if connection closed client side
-									if (nbBytesRead == -1) {
-										continue;
-									}
-									byte[] byteArray = buffer.array();
-									String content = new String(byteArray).trim();
-									System.out.println("Received message : \"" + content + "\", from : " + clientChannel.getRemoteAddress());
-								}
-
-								if(key.isWritable()) {
-									// send message
-								}
-							}
-						} catch (IOException | CancelledKeyException | ClosedSelectorException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-
-			});
+			handler = new Thread(new ServerHandler());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -107,8 +63,11 @@ public class ServerChannel {
 		handler.start();
 	}
 
+	/**
+	 * Stop the server handler and close remaining connections
+	 */
 	public void stopServer() {
-		System.out.println("Closing the server...");
+		System.out.println("Closing the server :");
 		isRunning = false;
 		try {
 			for (SelectionKey key : selector.keys()) {
@@ -116,12 +75,12 @@ public class ServerChannel {
 				if (channel.equals(serverChannel)) {
 					serverChannel.socket().close();			
 					serverChannel.close();
-					System.out.println("	Closing server socket");
+					System.out.println("	- Closing server socket");
 				}
 				else {
 					SocketChannel clientChannel = (SocketChannel) channel;
 					Socket clientSocket = clientChannel.socket();
-					System.out.println("	Closing connection to client " + clientSocket.getRemoteSocketAddress());
+					System.out.println("	- Closing connection to client " + clientSocket.getRemoteSocketAddress());
 					clientSocket.close();
 					clientChannel.close();
 				}
@@ -131,5 +90,119 @@ public class ServerChannel {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Handles connection and I/O with clients in an infinite loop, until stopped.
+	 * Works in a separate thread.
+	 * @author juju
+	 *
+	 */
+	class ServerHandler implements Runnable {
+
+		@Override
+		public void run() {
+			while(isRunning) {
+				try {
+					selector.select();
+					// checks whether the selector is open
+					if (!selector.isOpen()) {
+						break;
+					}
+					Set<SelectionKey> selectedKeys = selector.selectedKeys();
+					Iterator<SelectionKey> iterator = selectedKeys.iterator();
+
+					while (iterator.hasNext()) {
+						SelectionKey key = iterator.next();
+						iterator.remove();
+
+						// Accepting
+						if(key.isAcceptable()) {
+							acceptConnection(key);
+						}
+
+						// Connecting
+						if(key.isConnectable()) {
+							SocketChannel clientChannel = (SocketChannel) key.channel();
+							System.out.println("+ Finishing connection " + clientChannel.getRemoteAddress());
+							clientChannel.finishConnect();
+						}
+
+						// Reading
+						if(key.isValid() && key.isReadable()) {
+							readMessage(key);
+						}
+
+						// Writing
+						//if(!waitingMessages.isEmpty() && key.isValid() && key.isWritable()) {
+						if (messageReceived != null && key.isValid() && key.isWritable()) {
+							writeMessage(key);
+						}
+					}
+				} catch (IOException | CancelledKeyException | ClosedSelectorException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		/**
+		 * Pass on the message that was received from one client, to all clients
+		 * @param key : the destination of the message
+		 */
+		private void writeMessage(SelectionKey key) {
+			/*
+			 * TODO send the message ONCE to all clients
+			 * SocketChannel clientChannel = (SocketChannel) key.channel(); try { byte[]
+			 * messageBytes = messageReceived.getBytes(); ByteBuffer buffer =
+			 * ByteBuffer.wrap(messageBytes); clientChannel.write(buffer); Thread.sleep(10);
+			 * } catch (IOException | InterruptedException e) { e.printStackTrace(); }
+			 * System.out.println(">> "+ clientChannel.getRemoteAddress() + " : \"" + messageReceived + "\"");
+			 */
+		}
+
+		/**
+		 * Reads the message sent by one client
+		 * @param key : the source of the message
+		 */
+		private void readMessage(SelectionKey key) {
+			try {
+				SocketChannel clientChannel = (SocketChannel) key.channel();
+				ByteBuffer buffer = ByteBuffer.allocate(256);
+				int nbBytesRead = clientChannel.read(buffer);
+				// if connection closed client side => closing server side
+				if (nbBytesRead == -1) {
+					System.out.println("- Lost connection with client " + clientChannel.getRemoteAddress());
+					clientChannel.socket().close();
+					clientChannel.close();
+					key.cancel();
+					return;
+				}
+				// else => reading message
+				byte[] byteArray = buffer.array();
+				messageReceived = new String(byteArray).trim();
+				System.out.println("<< "+ clientChannel.getRemoteAddress() + " : \"" + messageReceived + "\"");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		/**
+		 * Accepts the connection requested by a client
+		 * @param key : the client
+		 */
+		private void acceptConnection(SelectionKey key) {
+			// creating new connection
+			SocketChannel clientChannel;
+			try {
+				clientChannel = serverChannel.accept();
+				clientChannel.configureBlocking(false);
+				// adding it to the selector
+				clientChannel.register(selector, clientChannel.validOps());
+				System.out.println("+ Added client " + clientChannel.getRemoteAddress());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
--- src/miniChat/TestMiniChat.java
+++ src/miniChat/TestMiniChat.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.Random;
 
 public class TestMiniChat {
 
@@ -16,27 +17,33 @@ public class TestMiniChat {
 		Client client2 = new Client(port);
 
 		// test messages client -> server
-		client1.sendMessage("1");
-		client1.sendMessage("-1");
-		client2.sendMessage("2");
+		client1.sendRandomMessages(2);	
+		client2.sendRandomMessages(2);
 		client2.close();
 		
-		client1.sendMessage("--1");
+		client1.sendRandomMessages(2);		
 
 		// wait a bit
-		Thread.sleep(5000);
-		
+		Thread.sleep(1000);
+
 		// close
 		//client2.close();
 		client1.close();
 		server.stopServer();
 	}	
 
+	
 
 	static class Client {
+		static int instanceCount = 0;
+		String name;
 		SocketChannel clientChannel;
+		Random generator;
 
 		public Client(int port) {
+			instanceCount++;
+			name = "client" + instanceCount;
+			generator = new Random();
 			try {
 				clientChannel = SocketChannel.open(new InetSocketAddress("localhost", port));
 				clientChannel.configureBlocking(false);
@@ -44,14 +51,46 @@ public class TestMiniChat {
 				e.printStackTrace();
 			}
 		}
+		
+		public Client(int port, String n) {
+			instanceCount++;
+			name = n;
+			generator = new Random();
+			try {
+				clientChannel = SocketChannel.open(new InetSocketAddress("localhost", port));
+				clientChannel.configureBlocking(false);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public void sendRandomMessages(int messageNumber) {
+			sendRandomMessages(messageNumber, 10);
+		}
+
+		public void sendRandomMessages(int messageNumber, int messageLength) {
+			byte[] symbols = "abcedfghijklomnpqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789".getBytes();
+			byte[] msg = new byte[messageLength];
+			for (int j = 0; j < messageNumber; j++) {
+				for (int i = 0; i < messageLength; i++) {
+					msg[i] = symbols[generator.nextInt(symbols.length)];				
+				}
+				ByteBuffer buffer = ByteBuffer.wrap(msg);
+				try {
+					clientChannel.write(buffer);
+					Thread.sleep(10);
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 		public void sendMessage(String msg) {
-			// FIXME sending messages too close to one another (before the server reads) melds them together
 			try {
 				byte[] message = msg.getBytes();
 				ByteBuffer buffer = ByteBuffer.wrap(message);
 				clientChannel.write(buffer);
-				Thread.sleep(5);
+				Thread.sleep(10);
 			} catch (IOException | InterruptedException e) {
 				e.printStackTrace();
 			}
